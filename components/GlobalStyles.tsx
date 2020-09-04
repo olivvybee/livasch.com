ad3263764f52d8e92a33bf4fa164923243c8ad2a
Olivia Asch <olivia.c.asch@gmail.com>
2020-09-04 19:46:06 +0100

    Add inline code styles

--- components/GlobalStyles.tsx
+++ components/GlobalStyles.tsx
@@ -73,6 +73,12 @@ const GlobalStyles = () => {
         ':not(:last-of-type)': {
           marginBottom: '1.5rem',
         },
+        code: {
+          fontFamily: theme.fontFamilyCode,
+          fontSize: '1.2rem',
+          backgroundColor: theme.colours.backgroundHighlight,
+          padding: 2,
+        },
       },
       h1: {
         fontSize: '2.5rem',
@@ -96,7 +102,7 @@ const GlobalStyles = () => {
         paddingLeft: 8,
         marginBottom: '1.5rem',
         p: {
-          background: theme.colours.backgroundAccent,
+          background: theme.colours.backgroundHighlight,
           padding: 16,
           paddingLeft: 24,
         },

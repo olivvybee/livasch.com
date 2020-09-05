63aaabde1e5193a0ca971d2669959a90185865f7
Olivia Asch <olivia.c.asch@gmail.com>
2020-09-05 11:39:35 +0100

    Add logo and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,21 @@ class Document extends NextDocument {
   render() {
     return (
       <Html lang='en-GB'>
-        <Head />
+        <Head>
+          <link rel='apple-touch-icon' href='apple-touch-icon.png' />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />

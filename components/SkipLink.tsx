7a43599dd536fbc92690ebae5f90df5f0b9b8930
Olivia Asch <olivia.c.asch@gmail.com>
2020-09-17 23:44:13 +0100

    Replace emotion with plain CSS in sr-only and skip link

--- components/SkipLink.tsx
+++ components/SkipLink.tsx
@@ -1,34 +1,13 @@
 import React from 'react';
 
-import { styled } from './Theming';
 import Link from './Link';
 
-const SkipLinkWrapper = styled('div')(({ theme }) => ({
-  position: 'absolute',
-  top: -100,
-  left: 8,
-  borderRadius: 8,
-  padding: 12,
-  backgroundColor: theme.colours.background,
-  border: `1px solid ${theme.colours.separator}`,
-
-  '@media not all and (prefers-reduced-motion: reduce)': {
-    transition: 'top 0.25s',
-  },
-
-  ':focus-within': {
-    top: 8,
-  },
-
-  a: {
-    color: theme.colours.text,
-  },
-}));
+import styles from '../styles/SkipLink.module.scss';
 
 const SkipLink = () => (
-  <SkipLinkWrapper>
+  <div className={styles.wrapper}>
     <Link href='#main-content'>Skip to content →</Link>
-  </SkipLinkWrapper>
+  </div>
 );
 
 SkipLink.displayName = 'SkipLink';

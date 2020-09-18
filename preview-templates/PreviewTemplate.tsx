3b02388501f27031a3b0e3be9e21bb853f6107fa
Olivia Asch <olivia.c.asch@gmail.com>
2020-09-18 15:40:54 +0100

    Copy CSS into Netlify CMS preview iframe

--- preview-templates/PreviewTemplate.tsx
+++ preview-templates/PreviewTemplate.tsx
@@ -1,5 +1,6 @@
 import createEmotionCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/core';
+import { useEffect } from 'react';
 
 interface PreviewTemplateProps {
   window: Window;
@@ -14,6 +15,31 @@ const PreviewTemplate: React.FC<PreviewTemplateProps> = ({
     container: document.head,
     key: 'preview-css',
   });
+
+  useEffect(() => {
+    const outerStylesheets = window.document.styleSheets;
+    const innerStylesheet = document.styleSheets[0];
+
+    // Number of stylesheets to ignore when copying into the iframe.
+    // These are added by the Netlify CMS so will always appear after
+    // the ones from Next.js because it loads asynchronously.
+    const netlifyStylesheets = 5;
+
+    for (
+      let sheet = 0;
+      sheet < outerStylesheets.length - netlifyStylesheets;
+      sheet++
+    ) {
+      console.log(outerStylesheets[sheet]);
+      const rulesToCopy = outerStylesheets[sheet].rules;
+      for (let rule = 0; rule < rulesToCopy.length; rule++) {
+        innerStylesheet.insertRule(
+          rulesToCopy[rule].cssText,
+          innerStylesheet.rules.length
+        );
+      }
+    }
+  }, []);
 
   return <CacheProvider value={emotionCache}>{children}</CacheProvider>;
 };
